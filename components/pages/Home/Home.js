778a9e8277917ecd94e8fdfb90bff346933fc785
Stephen Asuncion <stephenasuncion02@gmail.com>
2022-01-02 19:09:17 -0800

    Final commit before deployment

--- components/pages/Home/Home.js
+++ components/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Typography } from "@mui/material";
 import LogsContainer from "./LogsContainer"
--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ module.exports = {
         CHAIN_ID: process.env.CHAIN_ID,
         MORALIS_APP_ID: process.env.MORALIS_APP_ID,
         MORALIS_SERVER_URL: process.env.MORALIS_SERVER_URL,
-        PRIVATE_KEY: process.env.PRIVATE_KEY,
         METAMASK_ADDRESS: process.env.METAMASK_ADDRESS
     }
 }
- 
