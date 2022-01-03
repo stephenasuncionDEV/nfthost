bde07e79905ac50fd488a055d17bd20d65a9dbab
Stephen Asuncion <stephenasuncion02@gmail.com>
2022-01-02 19:24:33 -0800

    Fix 1

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology:true });
