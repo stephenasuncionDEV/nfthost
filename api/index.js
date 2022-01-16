fd79bc8c309d47bd70a58a8215b1e24c0e48f826
Stephen Asuncion <stephenasuncion02@gmail.com>
2022-01-16 15:20:58 -0800

    🧹 CLEAN: minor cleanups

--- api/index.js
+++ api/index.js
@@ -12,13 +12,6 @@ const connection = require('../db/connection');
 
 // Express Config
 app.options(cors());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/api', router);
--- middlewares/validators.js
+++ middlewares/validators.js
@@ -1,33 +1,30 @@
 const crypto = require('crypto');
+const { check } = require("express-validator");
 
-/**
- * Code from https://medium.com/@vampiire
- */
-
-const createComparisonSignature = (body) => {
-    const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECRET);
+const createComparisonSignature = (body, secret) => {
+    const hmac = crypto.createHmac('sha1', secret);
     const self_signature = hmac.update(JSON.stringify(body)).digest('hex');
-    return `sha1=${self_signature}`; // shape in GitHub header
+    return `sha1=${self_signature}`;
 }
 
 const compareSignatures = (signature, comparison_signature) => {
     const source = Buffer.from(signature);
     const comparison = Buffer.from(comparison_signature);
-    return crypto.timingSafeEqual(source, comparison); // constant time comparison
+    return crypto.timingSafeEqual(source, comparison);
 }
 
 module.exports.verifyGithubPayload = (req, res, next) => {
     const { headers, body } = req;
 
     const signature = headers['x-hub-signature'];
-    const comparison_signature = createComparisonSignature(body);
+    const comparison_signature = createComparisonSignature(body, process.env.WEBHOOK_SECRET);
 
     if (!compareSignatures(signature, comparison_signature)) {
         return res.status(401).send('Mismatched signatures');
     }
 
     const { action, ...payload } = body;
-    req.event_type = headers['x-github-event']; // one of: https://developer.github.com/v3/activity/events/types/ 
+    req.event_type = headers['x-github-event'];
     req.action = action;
     req.payload = payload;
     next();
