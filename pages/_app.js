d1ffb63f5e24394e21d6eded2470d2bd3051b5f5
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-05-28 21:29:50 -0700

    🌻UPDATE: added posthog

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 import '../styles/globals.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from '@/providers/UserProvider'
 import { GeneratorProvider } from '@/providers/GeneratorProvider'
 import { WebsiteProvider } from '@/providers/WebsiteProvider'
-import theme from '@/utils/theme'
 import { CoreProvider } from '@/providers/CoreProvider'
+import theme from '@/utils/theme'
+import posthog from 'posthog-js'
 
 const MyApp = ({ Component, pageProps }) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        posthog.init('phc_yllraz82JEawY3IhEhXl3yEJIKN4ueFJT66AYBc3tCe', { api_host: 'https://app.posthog.com' });
+
+        const handleRouteChange = () => {
+            posthog.capture('$pageview');
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [])
+
     return (
         <ChakraProvider theme={theme}>
             <CoreProvider>
