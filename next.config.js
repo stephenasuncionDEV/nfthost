4464be51b24f471cbcb55f42c5c3745b2ebc0624
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-05-28 21:51:26 -0700

    🌻UPDATE: fixed dependecy issue

--- next.config.js
+++ next.config.js
@@ -6,9 +6,14 @@ module.exports = {
         CHAIN_ID: process.env.CHAIN_ID,
         METAMASK_ADDRESS: process.env.METAMASK_ADDRESS,
         STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-        ENCRYPT_KEY: process.env.ENCRYPT_KEY
+        ENCRYPT_KEY: process.env.ENCRYPT_KEY,
+        POSTHOG_KEY: process.env.POSTHOG_KEY
     },
-    webpack: config => {
+    webpack: (config, { webpack }) => {
+        config.plugins.push(new webpack.IgnorePlugin({
+            resourceRegExp: /^electron$/
+        }));
+
         if (isDev) {
             return config;
         }
