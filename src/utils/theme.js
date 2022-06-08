6edac7ded23ae2f7de5f734f23b9bc6255feb042
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-06-08 02:22:49 -0700

    🌻UPDATE: fixes

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,7 +2,7 @@ import { extendTheme } from '@chakra-ui/react'
 import { mode } from "@chakra-ui/theme-tools"
 
 const config = {
-    initialColorMode: 'light',
+    initialColorMode: 'dark',
     useSystemColorMode: false,
 }
 
