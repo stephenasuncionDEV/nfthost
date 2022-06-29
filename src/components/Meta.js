cf12cd210e2b5c70377b0d27f61f96ef91eac834
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-06-29 03:39:47 -0700

    🌻UPDATE: fixed meta

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -9,13 +9,14 @@ const Meta = ({ title, children }) => {
         <meta name="robots" content='index, follow' />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content='en' />
-
+        <meta name="theme-color" content="#348CD4" />
+        
         <meta property="og:type" content='website' />
         <meta property="og:url" content='https://www.nfthost.app/' />
         <meta property="og:title" content='NFT Host - Generate &#38; Host your NFT Collection' />
         <meta property="og:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more.' />
         <meta property="og:image" content='https://www.nfthost.app/assets/logo.png' />
-        <meta name="theme-color" content="#348CD4" />
+        <meta property="og:site_name" content='NFT Host' />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content='https://www.nfthost.app/' />
