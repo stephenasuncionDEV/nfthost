810897bb5ec41c8106e5bcaea33a22a82dee538f
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-06-30 00:15:38 -0700

    🌻UPDATE: updates on get started page

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import { Flex, Wrap, Button, Text, Image, useColorModeValue, 
-    VStack, HStack, Link, Tag, TagLeftIcon 
+    VStack, HStack, Link, Tag, TagLeftIcon, Box
 } from '@chakra-ui/react'
 import { useGetStarted } from '@/hooks/useGetStarted'
 import { GiCutDiamond } from 'react-icons/gi'
@@ -97,10 +97,10 @@ const GetStarted = () => {
                             Socials
                         </Text>
                         <VStack spacing='1em' alignItems='flex-start'>
-                            <Link href='https://twitter.com/Steb_01' isExternal>
+                            <Link href='https://www.producthunt.com/posts/nft-host' isExternal>
                                 <HStack>
                                     <Text fontSize='10pt'>
-                                        Twitter
+                                        Product Hunt
                                     </Text>
                                     <FiExternalLink />
                                 </HStack>
@@ -113,10 +113,32 @@ const GetStarted = () => {
                                     <FiExternalLink />
                                 </HStack>
                             </Link>
+                            <Link href='https://twitter.com/Steb_01' isExternal>
+                                <HStack>
+                                    <Text fontSize='10pt'>
+                                        Twitter
+                                    </Text>
+                                    <FiExternalLink />
+                                </HStack>
+                            </Link>
                         </VStack>
                     </VStack>
                 </VStack>
             </Wrap>
+            <Flex flexDir='column' mt='2em'>
+                <Text fontWeight='bold' fontSize='26pt'>
+                    Support Us
+                </Text>
+                <Text fontSize='10pt'>
+                    If you want to support us, buy me a coffee or upvote NFT Host on Product Hunt.
+                </Text>
+                <Box mt='2em'>
+                    <a href="https://www.buymeacoffee.com/stephenasuncion" target="_blank"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=stephenasuncion&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
+                </Box>
+                <Box mt='1em'>
+                    <a href="https://www.producthunt.com/posts/nft-host?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-nft&#0045;host" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=326763&theme=dark" alt="NFT&#0032;Host - Generate&#0032;and&#0032;Host&#0032;your&#0032;NFT&#0032;Collection&#0032;in&#0032;under&#0032;10&#0032;minutes | Product Hunt" style={{width: '235px', height: '50px'}} width="235" height="50" /></a>
+                </Box>
+            </Flex>
         </Flex>
     )
 }
--- src/components/Meta.js
+++ src/components/Meta.js
@@ -4,7 +4,7 @@ const Meta = ({ title, children }) => {
     return <Head>
         <title>{title}</title>
         <meta name="title" content='NFT Host' />
-        <meta name="description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more.' />
+        <meta name="description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more. We also offer utils for your metadata files that includes metadata key updates.' />
         <meta name="keywords" content='NFT Host, Host NFT, Mint Website, Mint NFT Website Hosting, Mint NFT, NFT, Mint, Crypto Currency, Crypto, Ethereum' />
         <meta name="robots" content='index, follow' />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
@@ -14,14 +14,14 @@ const Meta = ({ title, children }) => {
         <meta property="og:type" content='website' />
         <meta property="og:url" content='https://www.nfthost.app/' />
         <meta property="og:title" content='NFT Host - Generate &#38; Host your NFT Collection' />
-        <meta property="og:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more.' />
+        <meta property="og:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more. We also offer utils for your metadata files that includes metadata key updates.' />
         <meta property="og:image" content='https://www.nfthost.app/assets/logo.png' />
         <meta property="og:site_name" content='NFT Host' />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content='https://www.nfthost.app/' />
         <meta property="twitter:title" content='NFT Host - Generate &#38; Host your NFT Collection' />
-        <meta property="twitter:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more.' />
+        <meta property="twitter:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. We provide the fastest and cheapest NFT generator in the market. Mint website features includes prebuilt templates, addons, domain, and more. We also offer utils for your metadata files that includes metadata key updates.' />
         <meta property="twitter:image" content='https://www.nfthost.app/assets/logo.png' />
 
         {children}
