3c0eca02a8c1b245b91f3eb9929a81cbbbc887a5
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-06-30 17:38:34 -0700

    🌻UPDATE: seperated backend from frontend

--- db/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology:true });
-const connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-module.exports = connection;
--- middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-module.exports.errorHandler = (err, req, res, next) => {
-    const { status } = err;
-    res.status(status | 500).json({status: err.status, message: err.message});
-}
