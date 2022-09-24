43012947a81fa601e1b0061dfb3c73265be43477
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-23 23:20:52 -0700

    🚀 UPDATE: updated helper text for logo

--- src/components/services/Website/Settings/Design.js
+++ src/components/services/Website/Settings/Design.js
@@ -80,7 +80,7 @@ const Design = () => {
                     <VStack spacing='.25em' alignItems='flex-start'>
                         <Text>Logo URL</Text>
                         <Text fontSize='10pt' variant='subtle'>
-                            An external link to an Icon file representing your website. This will be displayed on your browser's tab.
+                            An external link to an Image file representing your website. This will be displayed on your browser's tab.
                         </Text>
                     </VStack>
                     <DynamicInput 
