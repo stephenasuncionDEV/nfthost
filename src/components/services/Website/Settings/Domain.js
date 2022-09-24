a8512da7dfd9c95c573612bb75b54fa3dc4b3847
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-23 19:47:30 -0700

    ✨ NEW: added Domain component

---
+++ src/components/services/Website/Settings/Domain.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import { VStack, Button, Flex, Text, useColorModeValue } from '@chakra-ui/react'
+import { useWebsite } from '@/providers/WebsiteProvider'
+import { useWebsiteControls } from '@/hooks/services/website/useWebsiteControls'
+import DynamicInput from '@/components/DynamicInput'
+import { webColor } from '@/theme/index'
+
+const Domain = () => {
+    const { editingWebsite } = useWebsite();
+    const containerColor = useColorModeValue(webColor.containerBg[0], webColor.containerBg[1]);
+    const [route, setRoute] = useState('');
+
+    useEffect(() => {
+        if (!editingWebsite) return;
+        setRoute(editingWebsite.route);
+    }, [editingWebsite])
+
+    return (
+        <Flex flexDir='column' mt='1em' flex='1'>
+            <Flex 
+                flexDir='column' 
+                bg={containerColor} 
+                p='1em' 
+                borderRadius='.25em'
+                maxW='865px'
+            >
+                <Flex flexDir='column'>
+                    <VStack spacing='.25em' alignItems='flex-start'>
+                        <Text>Subdomain</Text>
+                        <Text fontSize='10pt' variant='subtle'>
+                            Where your NFT minters can navigate to mint your NFTs.
+                        </Text>
+                    </VStack>
+                    <DynamicInput 
+                        id='subdomain'
+                        name='subdomain'
+                        type='text'
+                        placeholder='Subdomain'
+                        value={route}
+                        onChange={setRoute}
+                        mt='1em'
+                        maxW='380px'
+                        addonRight
+                        addonRightText='.nfthost.app'
+                    />
+                </Flex>
+                <Flex justifyContent='flex-end' mt='1em'>
+                    <Button variant='primary'>
+                        Save
+                    </Button>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Domain
