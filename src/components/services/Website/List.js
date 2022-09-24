6ee9e29f75234c9d05551cef5136d5837b2083ce
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-23 19:49:13 -0700

    🚀 UPDATE: updated website list

--- src/components/services/Website/List.js
+++ src/components/services/Website/List.js
@@ -1,31 +1,26 @@
 import { HStack, Text, Button, VStack, IconButton,
-    useColorModeValue, Image, Wrap, Tag, TagLeftIcon
+    useColorModeValue, Wrap, Avatar, AvatarBadge, Flex, Box
 } from '@chakra-ui/react'
 import { MdRefresh, MdAdd } from 'react-icons/md'
-import { GiCutDiamond } from 'react-icons/gi'
 import { useWebsite } from '@/providers/WebsiteProvider'
 import { useWebsiteControls } from '@/hooks/services/website/useWebsiteControls'
 import { webColor } from '@/theme/index'
+import config from '@/config/index'
 
 const List = ({ onCreateWebsiteOpen }) => {
     const { 
         websites, 
-        isGettingWebsites, 
-        editingWebsite 
+        isGettingWebsites
     } = useWebsite();
-    const { getWebsites } = useWebsiteControls();
+    const { getWebsites, editWebsite } = useWebsiteControls();
 
     const containerColor = useColorModeValue(webColor.containerBg[0], webColor.containerBg[1]);
-    const buttonColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
-    const bgColor = useColorModeValue('rgba(0,0,0,0.1)', 'rgba(0,0,0,0.5)');
-    const buttonDefaultColor = useColorModeValue('gray.100', 'whiteAlpha.200');
 
     return (
         <VStack
             alignItems='flex-start'
             spacing='1.5em'
             p='1em' 
-            bg={containerColor}
             borderRadius='5px'
             boxShadow='0 0 2px 0 rgb(0 0 0 / 10%)'
             h='100%'
@@ -34,10 +29,10 @@ const List = ({ onCreateWebsiteOpen }) => {
             <HStack spacing='2em' justifyContent='space-between' w='full'>
                 <VStack spacing='0' alignItems='flex-start'>
                     <Text fontWeight='bold' fontSize='10pt'>
-                        Mint Websites
+                        Minting Websites
                     </Text>
                     <Text fontSize='10pt'>
-                        List of mint websites you own
+                        List of your minting websites
                     </Text>
                 </VStack>
                 <HStack>
@@ -55,39 +50,67 @@ const List = ({ onCreateWebsiteOpen }) => {
                     </IconButton>
                 </HStack>
             </HStack>
-            <Wrap spacing='1.5em' p='1em' bg={bgColor} borderRadius='10px' w='full'>
-                {websites?.map((website, idx) => (
-                    <Button 
-                        boxSize='180px'
-                        position='relative' 
-                        bgColor={buttonColor} 
-                        cursor='pointer' 
-                        borderRadius='10px'
-                        key={idx}
-                        overflow='hidden'
-                        borderColor={editingWebsite?._id === website._id ? 'rgb(52,140,212)' : buttonDefaultColor}
-                        borderBottomWidth='3px'
+            <Wrap spacing='1.5em' w='full' overflow='visible'>
+                {websites?.map((web, idx) => (
+                    <Flex 
+                        flexDir='column'
+                        key={idx} 
+                        w='380px'
+                        h='210px'
+                        bg={containerColor}
+                        p='1.5em'
+                        borderRadius='.25em'
+                        cursor='pointer'
+                        className='scaleAnimation'
+                        onClick={() => {
+                            editWebsite(web);
+                        }}
                     >
-                        <Image 
-                            position='absolute'
-                            src={website?.components?.unrevealedImage}
-                            alt='Website Logo' 
-                            objectFit='cover' 
-                            opacity='.1' 
-                            boxSize='250px'
-                        />
-                        {website.isPremium && (
-                            <Tag position='absolute' bottom='4'>
-                                <TagLeftIcon as={GiCutDiamond} color='#08BDD4' />
-                                <Text>
-                                    Premium
+                        <Flex flexDir='column' flex='1' w='full'>
+                            <HStack spacing='1em'>
+                                <Avatar 
+                                    src={web.components.unrevealedImage} 
+                                    name={`${web.components.title}'s logo`} 
+                                    bg='transparent'
+                                    size='sm'
+                                >
+                                    <AvatarBadge 
+                                        boxSize='1em' 
+                                        bg={web.isPublished ? 'green.500': 'red.500'} 
+                                        borderColor='white' 
+                                        borderWidth='1px' 
+                                    />
+                                </Avatar>
+                                <Flex flexDir='column' alignItems='flex-start'>
+                                    <Text fontWeight='bold'>
+                                        {web.components.title}
+                                    </Text>
+                                    <Text fontSize='10pt' variant='subtle'>
+                                        {`${config?.frontendUrl}/${web.custom.alias.length > 0 ? 
+                                            web.custom.alias 
+                                            : 
+                                            web.route}
+                                        `}
+                                    </Text>
+                                </Flex>
+                            </HStack>
+                            <Box flex='1' w='full'>
+                                <Text 
+                                    fontSize='10pt' 
+                                    mt='2em' 
+                                    noOfLines={3}
+                                    textAlign='start'
+                                >
+                                    {web.components.description}
                                 </Text>
-                            </Tag>
-                        )}
-                        <Text maxW='140px' noOfLines={1}>
-                            {website.components.title}
-                        </Text>
-                    </Button>
+                            </Box>
+                        </Flex>
+                        <Flex>
+                            <Text fontSize='9pt' variant='subtle'>
+                                {Math.floor((new Date() - Date.parse(web.createdAt)) / 86400000).toString()}d ago
+                            </Text>              
+                        </Flex>
+                    </Flex>
                 ))}
             </Wrap>
         </VStack>
