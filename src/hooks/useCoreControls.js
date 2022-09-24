f9279a202ecda2e9f850468a517f150d4b77a066
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-23 23:19:10 -0700

    ✨ NEW: added core control hook

---
+++ src/hooks/useCoreControls.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { getAccessToken } from '@/utils/tools'
+import axios from 'axios'
+import config from '@/config/index'
+
+export const useCoreControls = () => {
+    const [featuredWebsites, setFeaturedWebsites] = useState([]);
+
+    const getFeaturedWebsites = async () => {
+        try {
+            const accessToken = getAccessToken();
+
+            const res = await axios.get(`${config.serverUrl}/api/core/getFeaturedWebsites`, {
+                headers: { 
+                    Authorization: `Bearer ${accessToken}` 
+                }
+            })
+
+            console.log(res.data)
+
+            setFeaturedWebsites(res.data)
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    return {
+        featuredWebsites,
+        getFeaturedWebsites
+    }
+}
