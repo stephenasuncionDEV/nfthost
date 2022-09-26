8c9d9a8928f3b60e88e83a7f0f71ecd0bba3e7de
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-09-26 04:18:15 -0700

    🐛 FIX: vercel fix 17

--- middleware.js
+++ middleware.js
@@ -10,13 +10,13 @@ export default async function middleware(req) {
 
     const subpath = process.env.NODE_ENV === 'production' ? hostname.slice(0, hostname.indexOf('.')) : 'www';
 
-    if (url.pathname.startsWith(`/mints`)) {
-        url.pathname = `/404`
-    } else {
+    // if (url.pathname.startsWith(`/mints`)) {
+    //     url.pathname = `/404`
+    // } else {
         if (subpath !== 'www') {   
             url.pathname = `/mints/${subpath}${url.pathname}`;
         }
-    }
+    //}
 
     return NextResponse.rewrite(url);
 }
