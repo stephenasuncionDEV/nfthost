bb1efd74d5fb62efd161326104fd6ef9eb5ad643
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-05 21:00:20 -0700

    🐛 FIX: fixed meta grammar

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -5,7 +5,7 @@ const Meta = ({ title }) => {
         <title>{title}</title>
         <link rel="shortcut icon" type="image/png" href='https://www.nfthost.app/favicon.ico' />
         <meta name="title" content='NFT Host' />
-        <meta name="description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. Metadata utilities available for post-generation.' />
+        <meta name="description" content='NFT Host is a website where you can generate NFT collections and create an NFT minting website. Metadata utilities available for post-generation.' />
         <meta name="keywords" content='NFT Host, Host NFT, Mint Website, Mint NFT Website Hosting, Mint NFT, NFT, Mint, Crypto Currency, Crypto, Ethereum' />
         <meta name="robots" content='index, follow' />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
@@ -15,7 +15,7 @@ const Meta = ({ title }) => {
         <meta property="og:type" content='website' />
         <meta property="og:url" content='https://www.nfthost.app/' />
         <meta property="og:title" content='NFT Host - Generate &#38; Host your NFT Collection' />
-        <meta property="og:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. Metadata utilities available for post-generation.' />
+        <meta property="og:description" content='NFT Host is a website where you can generate NFT collections and create an NFT minting website. Metadata utilities available for post-generation.' />
         <meta property="og:image" content='https://www.nfthost.app/assets/nfthost-og.png' />
         <meta property="og:image:alt" content='NFTHost OpenGraph Picture' />
         <meta property="og:site_name" content='NFT Host' />
@@ -23,7 +23,7 @@ const Meta = ({ title }) => {
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content='https://www.nfthost.app/' />
         <meta property="twitter:title" content='NFT Host - Generate &#38; Host your NFT Collection' />
-        <meta property="twitter:description" content='NFT Host is a website where you can generate NFT collections and create NFT minting website. Metadata utilities available for post-generation.' />
+        <meta property="twitter:description" content='NFT Host is a website where you can generate NFT collections and create an NFT minting website. Metadata utilities available for post-generation.' />
         <meta property="twitter:image" content='https://www.nfthost.app/assets/nfthost-og.png' />
         <meta property="twitter:image:alt" content='NFTHost TwitterCard Picture' />
     </Head>
