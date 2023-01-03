2c804b07f7ff9d1ca05b35f2d6c590295152b2c3
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-01-02 22:16:59 -0800

    🚀 UPDATE: updated cookie modal style

--- src/components/CookieModal.js
+++ src/components/CookieModal.js
@@ -10,12 +10,16 @@ import {
 import { IoMdClose } from "@react-icons/all-files/io/IoMdClose";
 import { useCore } from "@/providers/CoreProvider";
 import { useCookie } from "@/hooks/useCookie";
+import { webColor } from "@/theme/index";
 
 const CookieModal = () => {
   const { isCookieModal, setIsCookieModal } = useCore();
   const { Accept } = useCookie();
 
-  const containerColor = useColorModeValue("white", "blackAlpha.800");
+  const borderColor = useColorModeValue(
+    webColor.borderColor[0],
+    webColor.borderColor[1],
+  );
 
   return (
     isCookieModal && (
@@ -23,32 +27,37 @@ const CookieModal = () => {
         position="fixed"
         bottom="3em"
         right="3em"
-        h="250px"
-        bg={containerColor}
+        h="220px"
+        bg="#000616"
         w="400px"
         p="2em"
-        fontWeight="bold"
         borderRadius="10px"
         flexDir="column"
         justifyContent="center"
         boxShadow="0px 0px 50px -11px rgba(0,0,0,0.3)"
         zIndex="1337"
+        border={`1px solid ${borderColor}`}
       >
         <HStack justifyContent="space-between">
           <Text variant="content_subtitle">Cookies &#38; Privacy</Text>
-          <IconButton onClick={() => setIsCookieModal(false)}>
+          <IconButton
+            onClick={() => setIsCookieModal(false)}
+            variant="transparent"
+          >
             <IoMdClose />
           </IconButton>
         </HStack>
-        <Text mt="1.5em">This website uses cookies to ensure you</Text>
-        <Text>get the best experience on our website.</Text>
+        <Text mt="1.5em" fontSize="10pt">
+          This website uses cookies to ensure you
+        </Text>
+        <Text fontSize="10pt">get the best experience on our website.</Text>
         <Flex w="full" justifyContent="flex-end" alignItems="flex-end" mt="2em">
           <Link href="/about/privacy-policy#cookies" isExternal>
             <Text variant="link" fontSize="10pt">
               More Information
             </Text>
           </Link>
-          <Button ml="1em" onClick={Accept}>
+          <Button ml="1em" onClick={Accept} variant="outline" size="sm">
             Accept
           </Button>
         </Flex>
