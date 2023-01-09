6f019276f3f85342bc07eab6d61ddd464e31398d
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-01-08 16:51:10 -0800

    🎨 FORMAT: formatted

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.VERCEL_URL || "http://localhost:3000/",
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: "*",
-          allow: "/",
-        },
-      ],
-    },
-  };
-  
+  siteUrl: process.env.VERCEL_URL || "http://localhost:3000/",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+};
