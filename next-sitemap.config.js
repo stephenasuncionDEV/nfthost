589193cf1811bfef637d0a9d80a9fd4249fadf20
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-01-08 16:43:48 -0800

    ✨ NEW: added sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.VERCEL_URL || "http://localhost:3000/",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+    },
+  };
+  
